40f27ae143e37376248d064121ae9c7349efaaaf
Ferry To <ferrywlto@gmail.com>
2025-11-10 23:43:52 +0000

    feat: Use XAML to for widget related controls.

--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -3,26 +3,26 @@
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	string currentString = "Hello, World!";
 
 	public MainPage()
 	{
 		InitializeComponent();
-        var writeBtn = new Button { Text = "Write & Reload Widget" };
-        writeBtn.Clicked += (s, e) =>
-        {
-#if IOS
-            count++;
-            WidgetBridge.WriteHello($"Hello #{count} at {DateTime.Now:T}");
-            // WidgetBridge.ReloadAllViaSwift(); // or .ReloadAll() if you have direct binding
-#endif
-        };
-
-        Content = new VerticalStackLayout
-        {
-            Children = { new Label { Text = "MAUI ↔︎ Widget PoC" }, writeBtn }
-        };		
+		WidgetLabel.Text = currentString;
 	}
 
+	private void OnWriteAndReloadClicked(object sender, EventArgs e)
+	{
+		#if IOS
+			count++;
+			currentString = $"Hello #{count} at {DateTime.Now:T}";
+			WidgetBridge.WriteHello(currentString);
+			// WidgetBridge.ReloadAllViaSwift(); // or .ReloadAll() if you have direct binding
+		#endif        
+		
+		WidgetLabel.Text = currentString;
+		SemanticScreenReader.Announce(WidgetLabel.Text);
+    }
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
